4b22b9c9afbc11dc565bba433d58283742a23b5a
unknown <abhisheklfps@gmail.com>
2024-11-20 02:32:31 +0530

    revision-y

--- DA1/find_s_algorithm.py
+++ DA1/find_s_algorithm.py
@@ -3,14 +3,9 @@ import pandas as pd
 df = pd.read_csv('FindS.csv')
 
 def find_s_algorithm(df):
-    # set all initial hypotheses to 0
-    hypothesis = ['0'] * (6)
-    
+    hypothesis = ['0'] * (len(df.columns))
     for i in range(len(df)):
-        # consider only the positive instance
         if df.iloc[i, -1] == 'Yes':  
-            # compare the hypotheses with the actual values, if its equal then its satisfied so nothing to do
-            # if its not equal then we need to change the hypothesis to the more general constraint
             for j in range(len(hypothesis)):
                 actual_term = df.iloc[i, j]
                 if hypothesis[j] == '0':  
